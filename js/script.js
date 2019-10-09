04209be2df28195ff1a9ebd9d2e9a7db3203cd45
xamsuka <v.kozlovsky97@gmail.com>
2019-10-09 22:57:48 +0600

    Доработка

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// var linkSearch = document.querySelector(".button-search__link");
+// var popapSearch = document.querySelector(".search");
+// var linkLogin = document.querySelector(".button-login__link");
+// var popapLogin = document.querySelector(".login");
+// var linkBasket= document.querySelector(".button-basket__link");
+// var popapBasket = document.querySelector(".basket");
+
+
+
+// linkSearch.addEventListener("click",function (evt) {
+//     evt.preventDefault();
+//     popapSearch.classList.toggle("display-none");
+// });
+
+// linkLogin.addEventListener("click",function (evt) {
+//     evt.preventDefault();
+//     popapLogin.classList.toggle("display-none");
+// });
+
+// linkBasket.addEventListener("click",function (evt) {
+//     evt.preventDefault();
+//     popapBasket.classList.toggle("display-none");
+// });
+
