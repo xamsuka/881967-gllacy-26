8a371db1a90d4a66e6e705bb81dd1ef001756d44
xamsuka <v.kozlovsky97@gmail.com>
2019-10-14 17:31:52 +0600

    Добавление js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var feedback = document.querySelector('.feedback');
 var feedbackButton = document.querySelector('.map-contact-block__button');
-var feedbackButtonClose = document.querySelector('.feedback-form__close')
+var feedbackButtonClose = document.querySelector('.feedback-form__close');
+var login = document.querySelector('.button-login__link');
+var loginInput = document.querySelector('.login-form__input');
+var search = document.querySelector('.button-search__link');
+var searchButton = document.querySelector('.search-form__input');
 
 feedbackButton.addEventListener("click", function(evt) {
     evt.preventDefault();
@@ -13,3 +17,11 @@ feedbackButtonClose.addEventListener("click", function(evt){
 
     feedback.classList.toggle('display-show');
 });
+
+login.addEventListener("mouseover", function (){
+    loginInput.focus();
+});
+
+search.addEventListener("mouseover", function() {
+    searchButton.focus();
+})
