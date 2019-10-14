971e4ebbb0f749f50c6f0160ae9fb9cd1bc7142a
xamsuka <v.kozlovsky97@gmail.com>
2019-10-14 13:30:52 +0600

    Форма обратной связи

--- js/script.js
+++ js/script.js
@@ -1,24 +1,15 @@
-// var linkSearch = document.querySelector(".button-search__link");
-// var popapSearch = document.querySelector(".search");
-// var linkLogin = document.querySelector(".button-login__link");
-// var popapLogin = document.querySelector(".login");
-// var linkBasket= document.querySelector(".button-basket__link");
-// var popapBasket = document.querySelector(".basket");
+var feedback = document.querySelector('.feedback');
+var feedbackButton = document.querySelector('.map-contact-block__button');
+var feedbackButtonClose = document.querySelector('.feedback-form__close')
 
+feedbackButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
 
+    feedback.classList.add('display-show');
+});
 
-// linkSearch.addEventListener("click",function (evt) {
-//     evt.preventDefault();
-//     popapSearch.classList.toggle("display-none");
-// });
+feedbackButtonClose.addEventListener("click", function(evt){
+    evt.preventDefault();
 
-// linkLogin.addEventListener("click",function (evt) {
-//     evt.preventDefault();
-//     popapLogin.classList.toggle("display-none");
-// });
-
-// linkBasket.addEventListener("click",function (evt) {
-//     evt.preventDefault();
-//     popapBasket.classList.toggle("display-none");
-// });
-
+    feedback.classList.toggle('display-show');
+});
